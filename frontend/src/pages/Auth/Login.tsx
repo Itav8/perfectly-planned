d834cb41cac3824015be09c009f4722bdda97607
Italiz <italizv@gmail.com>
2023-08-09 12:05:03 -0400

    auth setup

--- frontend/src/pages/Auth/Login.tsx
+++ frontend/src/pages/Auth/Login.tsx
@@ -1,7 +1,43 @@
+import { useEffect } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import * as firebaseui from "firebaseui";
+
 export const Login = () => {
+  const setupAuth = () => {
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyBodTJni4xSli2NfAS3BE-DI4TK400AlEY",
+      authDomain: "perfectly-planned-395119.firebaseapp.com",
+      projectId: "perfectly-planned-395119",
+      storageBucket: "perfectly-planned-395119.appspot.com",
+      messagingSenderId: "447075764646",
+      appId: "1:447075764646:web:e255b546a04501afb9e767",
+      measurementId: "G-1TC4219P80",
+    };
+    const app = firebase.initializeApp(firebaseConfig);
+    const ui = new firebaseui.auth.AuthUI(app.auth());
+
+    ui.start("#firebaseui-auth-container", {
+      signInOptions: [
+        {
+          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          requireDisplayName: false,
+        },
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      ],
+    });
+  };
+
+  useEffect(() => {
+    setupAuth();
+  }, []);
+
   return (
     <div>
       <h1>Login Page</h1>
+      <div id="firebaseui-auth-container"></div>
+      <div id="loader">Loading...</div>
     </div>
   );
 };
