367fae671e4de9cd0508a5d8f847fda934a95a65
Jerry Chen <misodope@cap-rx.com>
2023-08-09 13:01:04 -0400

    Make auth work

--- frontend/src/pages/Auth/Login.tsx
+++ frontend/src/pages/Auth/Login.tsx
@@ -1,35 +1,40 @@
 import { useEffect } from "react";
 import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
 import * as firebaseui from "firebaseui";
+import "firebase/compat/auth";
+import "firebaseui/dist/firebaseui.css";
 
 export const Login = () => {
-  const setupAuth = () => {
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-      apiKey: "AIzaSyBodTJni4xSli2NfAS3BE-DI4TK400AlEY",
-      authDomain: "perfectly-planned-395119.firebaseapp.com",
-      projectId: "perfectly-planned-395119",
-      storageBucket: "perfectly-planned-395119.appspot.com",
-      messagingSenderId: "447075764646",
-      appId: "1:447075764646:web:e255b546a04501afb9e767",
-      measurementId: "G-1TC4219P80",
-    };
-    const app = firebase.initializeApp(firebaseConfig);
-    const ui = new firebaseui.auth.AuthUI(app.auth());
-
-    ui.start("#firebaseui-auth-container", {
-      signInOptions: [
-        {
-          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          requireDisplayName: false,
-        },
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      ],
-    });
-  };
-
   useEffect(() => {
+    const setupAuth = () => {
+      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+      const firebaseConfig = {
+        apiKey: "AIzaSyBodTJni4xSli2NfAS3BE-DI4TK400AlEY",
+        authDomain: "perfectly-planned-395119.firebaseapp.com",
+        projectId: "perfectly-planned-395119",
+        storageBucket: "perfectly-planned-395119.appspot.com",
+        messagingSenderId: "447075764646",
+        appId: "1:447075764646:web:e255b546a04501afb9e767",
+        measurementId: "G-1TC4219P80",
+      };
+
+      const app = firebase.initializeApp(firebaseConfig);
+
+      let ui = firebaseui.auth.AuthUI.getInstance();
+
+      if (!ui) ui = new firebaseui.auth.AuthUI(app.auth());
+
+      ui.start("#firebaseui-auth-container", {
+        signInOptions: [
+          {
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            requireDisplayName: false,
+          },
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+      });
+    };
+
     setupAuth();
   }, []);
 
@@ -37,7 +42,6 @@ export const Login = () => {
     <div>
       <h1>Login Page</h1>
       <div id="firebaseui-auth-container"></div>
-      <div id="loader">Loading...</div>
     </div>
   );
 };
