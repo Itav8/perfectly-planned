bcd0cfc47c144e1148fe935b76876b029879dce8
Jerry Chen <misodope@cap-rx.com>
2023-08-16 10:01:34 -0400

    add landing

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -1,7 +1,21 @@
+import "./Landing.css";
+
 export const Landing = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <div className="landing">
+      <video muted autoPlay loop>
+        <source
+          type="video/mp4"
+          src="https://misodope.s3.amazonaws.com/perfectly-planned-bg-vid.mp4"
+        />
+      </video>
+      <div className="landing-content">
+        <h1>
+          Make all the Celebrations along the way <br />
+          <em>Perfectly Planned.</em>{" "}
+        </h1>
+        <button className="landing-button">Get Started</button>
+      </div>
     </div>
   );
 };
