5565632213b21a5bdda070ab8dea49f3d8f3db9a
Italiz <italizv@gmail.com>
2023-09-05 20:15:36 -0400

    updated responsive modal forms

--- frontend/src/pages/Events/WeddingForm.tsx
+++ frontend/src/pages/Events/WeddingForm.tsx
@@ -161,133 +161,132 @@ export const WeddingForm = (props: WeddingFormProps) => {
   };
 
   return (
-    <div>
+    <div className="guest-form__container">
       <h1>{props.type === "create" ? "Create Wedding" : "Edit Wedding"}</h1>
-      <div>
-        <form
-          onSubmit={(event) => {
-            if (props.type === "create") {
-              handleSubmit(event);
-            } else {
-              handleEditSubmit(event, weddingForm.wedding_id);
-            }
-          }}
-        >
-          <div>
-            <label htmlFor="wedding_name"> Wedding Name:</label>
-            <input
-              type="text"
-              id="wedding_name"
-              name="wedding_name"
-              value={weddingForm.wedding_name}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="wedding_date">Wedding Date:</label>
-            <input
-              type="datetime-local"
-              id="wedding_date"
-              name="wedding_date"
-              value={weddingForm.wedding_date}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="wedding_theme">Wedding Theme:</label>
-            <input
-              type="text"
-              id="wedding_theme"
-              name="wedding_theme"
-              value={weddingForm.wedding_theme}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="wedding_budget">Wedding Budget:</label>
-            <input
-              type="number"
-              id="wedding_budget"
-              name="wedding_budget"
-              value={weddingForm.wedding_budget}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="wedding_guest">Wedding Guests:</label>
-            <input
-              type="number"
-              id="wedding_guest"
-              name="wedding_guest"
-              value={weddingForm.wedding_guest}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="wedding_venue">Wedding Venue:</label>
-            <input
-              type="text"
-              id="wedding_venue"
-              name="wedding_venue"
-              value={weddingForm.wedding_venue}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="wedding_decorations">Wedding Decorations:</label>
-            <input
-              type="text"
-              id="wedding_decorations"
-              name="wedding_decorations"
-              value={weddingForm.wedding_decorations}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="wedding_registry">Wedding Registry:</label>
-            <input
-              type="text"
-              id="wedding_registry"
-              name="wedding_registry"
-              value={weddingForm.wedding_registry}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="wedding_planner">Wedding Planner:</label>
-            <input
-              type="checkbox"
-              id="wedding_planner"
-              name="wedding_planner"
-              checked={weddingForm.wedding_planner}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="wedding_photographer">Wedding Photographer:</label>
-            <input
-              type="text"
-              id="wedding_photographer"
-              name="wedding_photographer"
-              value={weddingForm.wedding_photographer}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="completed">Completed:</label>
-            <input
-              type="checkbox"
-              id="completed"
-              name="completed"
-              checked={weddingForm.completed}
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <button>Submit</button>
-          </div>
-        </form>
-      </div>
+      <form
+        className="guest-form"
+        onSubmit={(event) => {
+          if (props.type === "create") {
+            handleSubmit(event);
+          } else {
+            handleEditSubmit(event, weddingForm.wedding_id);
+          }
+        }}
+      >
+        <div>
+          <label htmlFor="wedding_name"> Wedding Name:</label>
+          <input
+            type="text"
+            id="wedding_name"
+            name="wedding_name"
+            value={weddingForm.wedding_name}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="wedding_date">Wedding Date:</label>
+          <input
+            type="datetime-local"
+            id="wedding_date"
+            name="wedding_date"
+            value={weddingForm.wedding_date}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="wedding_theme">Wedding Theme:</label>
+          <input
+            type="text"
+            id="wedding_theme"
+            name="wedding_theme"
+            value={weddingForm.wedding_theme}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="wedding_budget">Wedding Budget:</label>
+          <input
+            type="number"
+            id="wedding_budget"
+            name="wedding_budget"
+            value={weddingForm.wedding_budget}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="wedding_guest">Wedding Guests:</label>
+          <input
+            type="number"
+            id="wedding_guest"
+            name="wedding_guest"
+            value={weddingForm.wedding_guest}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="wedding_venue">Wedding Venue:</label>
+          <input
+            type="text"
+            id="wedding_venue"
+            name="wedding_venue"
+            value={weddingForm.wedding_venue}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="wedding_decorations">Wedding Decorations:</label>
+          <input
+            type="text"
+            id="wedding_decorations"
+            name="wedding_decorations"
+            value={weddingForm.wedding_decorations}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="wedding_registry">Wedding Registry:</label>
+          <input
+            type="text"
+            id="wedding_registry"
+            name="wedding_registry"
+            value={weddingForm.wedding_registry}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div className="guest-form__checkbox">
+          <label htmlFor="wedding_planner">Wedding Planner:</label>
+          <input
+            type="checkbox"
+            id="wedding_planner"
+            name="wedding_planner"
+            checked={weddingForm.wedding_planner}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="wedding_photographer">Wedding Photographer:</label>
+          <input
+            type="text"
+            id="wedding_photographer"
+            name="wedding_photographer"
+            value={weddingForm.wedding_photographer}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div className="guest-form__checkbox">
+          <label htmlFor="completed">Completed:</label>
+          <input
+            type="checkbox"
+            id="completed"
+            name="completed"
+            checked={weddingForm.completed}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
     </div>
   );
 };
